6b2e7475f384569112efcf698e84dc95e6906f59
Luciano Araujo <luchix1@outlook.es>
2025-12-09 20:44:36 -0300

    contacto fomulario

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -35,7 +35,7 @@ class ContactController extends Controller
             'message' => $message,
         ]);
 
-        $to      = 'contacto@nocturnaclothing.store';
+        $to      = 'formulariopagina@nocturnaclothing.store';
         $mailSub = 'Nuevo mensaje desde Nocturna: ' . $finalSubject;
 
         $body  = "Te escribió desde la web Nocturna:\n\n";
